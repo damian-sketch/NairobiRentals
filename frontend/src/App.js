4292f6fc45543b78b07c9d2bc8f866cf42b0bb5a
Cedric Muuo <cedric.muuo@tradecoreprime.com>
2022-02-14 01:27:22 +0300

    merge conflict resolved message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
 import { useEffect, useState} from 'react'
+import Registration from './components/auth/registration'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  const [users, setUsers] = useState([])
-  const getData = async() => {
-    const res = await axios.get('/users')
-    setUsers(res.data)
-  }
+  // const [users, setUsers] = useState([])
+  // const getData = async() => {
+  //   const res = await axios.get('/api/users')
+  //   setUsers(res.data)
+  // }
 
-  useEffect(() => {
-    getData()
-  }, [])
+  // useEffect(() => {
+  //   getData()
+  // }, [])
 
   return (
-    <div>
-      {users.map(u => <h4 key={u._id}>userName : {u.userName}</h4>)}
-    </div>
+    <Registration/>
   )
 }
 
