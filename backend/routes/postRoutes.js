0db9058d1dfdb3e04fdb0bf92338068963ada808
Cedric Muuo <cedric.muuo@tradecoreprime.com>
2022-06-10 17:50:38 +0300

    Removing ignored files

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { addHouse, getHouse } from "../controllers/houseControllers.js";
-import { upload } from "../middlewares/multer.js";
 const router = express.Router();
 
 // route to submit a new post
