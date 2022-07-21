2c6e33484bcf1b04e99947f79e89d8df3fdc5f81
Cedric Muuo <cedric.muuo@tradecoreprime.com>
2022-07-21 14:50:13 +0300

    Rectify cors url

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,33 @@
-import connectDB from './config/db.js'
-import dotenv  from 'dotenv'
-import userRoutes from './routes/userRoutes.js'
-import express from 'express'
-import authRoutes from './routes/authRoutes.js'
-import bodyParser from 'body-parser'
-import cors from 'cors';
-import postRoutes from './routes/postRoutes.js'
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import bodyParser from "body-parser";
+import cors from "cors";
+import postRoutes from "./routes/postRoutes.js";
 
 //connect db
-connectDB()
+connectDB();
 
-dotenv.config()
+dotenv.config();
 
-const app =express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: 'http://localhost:3000'
-}))
-app.use(bodyParser.json())
+    origin: "https://househunters-express-server.herokuapp.com/",
+  })
+);
+app.use(bodyParser.json());
 //routes
-app.use('/users', userRoutes)
-app.use('/', authRoutes)
-app.use('/posts', postRoutes)
+app.use("/users", userRoutes);
+app.use("/", authRoutes);
+app.use("/posts", postRoutes);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`App is runnning in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`App is runnning in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
