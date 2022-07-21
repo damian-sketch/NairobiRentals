18f48b68d7c323d471a00f0e56b4ef7ac5a10199
Cedric Muuo <cedric.muuo@tradecoreprime.com>
2022-07-21 13:54:21 +0300

    Install bcryptjs

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,6 @@
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import envVariables from "../middlewares/envVariables.js";
 
