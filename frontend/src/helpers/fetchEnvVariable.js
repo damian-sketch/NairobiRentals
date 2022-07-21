4fd7af666fffdce02ba6405ddd031defc81e437f
Cedric Muuo <cedric.muuo@tradecoreprime.com>
2022-07-21 14:28:05 +0300

    Connect frontend to depoloyed backend

--- frontend/src/helpers/fetchEnvVariable.js
+++ frontend/src/helpers/fetchEnvVariable.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/";
+const API_URL = "https://househunters-express-server.herokuapp.com/";
 
 export default async function fetchEnvVariable(variable) {
   return await axios.get(API_URL + "variables", {
--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/";
+const API_URL = "https://househunters-express-server.herokuapp.com/";
 export let success;
 export let resonse;
 class AuthService {
